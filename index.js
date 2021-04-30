cf9a22c0a602af8fa101625f91c1dcded600a18b
Victoria <victoriathorsenn@gmail.com>
2021-04-30 16:28:36 -0400

    commit tree 1e152a53c1c41a6da90bf69b69d6806f9f527b9b

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  constructor(string){
+    this.string = string;
+  }
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '');
+    
+  }
+
+  static titleize(string){
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = []
+    let words = string.split(" ")
+    for (let i = 0; i < words.length; i++)
+      if (i == 0) {
+        result.push(words[i].charAt(0).toUpperCase() + words[i].slice(1))
+      } else {
+          if (exceptions.includes(words[i])) {
+            result.push(words[i])
+          } else {
+            result.push(words[i].charAt(0).toUpperCase() + words[i].slice(1))
+          }
+      }
+      return result.join(" ")
+  }
 }
